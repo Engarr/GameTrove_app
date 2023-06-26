d7e452d2996aa5b303a18ab3c0bb9b7202d031c3
Łukasz <lukasz.poczta12@gmail.com>
2023-06-26 19:05:19 +0200

    styled NavLeftBar

--- src/components/Nav/NavBarLeft/NavBarLeft.tsx
+++ src/components/Nav/NavBarLeft/NavBarLeft.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
 import classes from './NavBarLeft.module.scss';
 import Modal from '../../Modal/Modal';
+import { gameCategories, gamePlatforms } from '../../../util/db';
 
 const NavBarLeft = () => {
-  const [isAcite, setIsAcite] = useState(false);
+  const [isAcite, setIsAcite] = useState(true);
   const activeHandler = () => {
     setIsAcite((prev) => !prev);
   };
@@ -16,9 +17,23 @@ const NavBarLeft = () => {
             isAcite ? classes.active : ''
           }`}
         >
-          <div>
-            <p>test</p>
+          <h3>Serach games by category:</h3>
+          <div className={classes[`nav__container--categorys`]}>
+            {gameCategories.map((category) => (
+              <div key={category.id}>
+                <button type="button">{category.category}</button>
+              </div>
+            ))}
           </div>
+          <h3>Serach games by platform:</h3>
+          <div className={classes[`nav__container--platforms`]}>
+            {gamePlatforms.map((category) => (
+              <div key={category.id}>
+                <button type="button">{category.platform}</button>
+              </div>
+            ))}
+          </div>
+
           <IoIosArrowForward
             className={`${classes.arrow} ${isAcite ? classes.arrowRotate : ''}`}
             onClick={activeHandler}
---
+++ src/util/db.tsx
@@ -0,0 +1,36 @@
+interface CategoryType {
+  id: number;
+  category: string;
+}
+interface PlatformType {
+  id: number;
+  platform: string;
+}
+export const gameCategories: CategoryType[] = [
+  { id: 1, category: 'Action' },
+  { id: 2, category: 'Adventure' },
+  { id: 3, category: 'Shooter' },
+  { id: 4, category: 'RPG (Role-Playing Game)' },
+  { id: 5, category: 'Platform' },
+  { id: 6, category: 'Racing' },
+  { id: 7, category: 'Sports' },
+  { id: 8, category: 'Simulation' },
+  { id: 9, category: 'Strategy' },
+  { id: 10, category: 'Fighting' },
+  { id: 11, category: 'Survival' },
+  { id: 12, category: 'Horror' },
+  { id: 13, category: 'Puzzle' },
+  { id: 14, category: 'MMO (Massively Multiplayer Online)' },
+  { id: 15, category: 'Narrative' },
+  { id: 16, category: 'Stealth' },
+  { id: 17, category: 'Roguelike' },
+  { id: 18, category: 'Indie' },
+];
+
+export const gamePlatforms: PlatformType[] = [
+  { id: 1, platform: 'PC' },
+  { id: 2, platform: 'PlayStation' },
+  { id: 3, platform: 'Xbox' },
+  { id: 4, platform: 'Nintendo Switch' },
+  // Dodaj tutaj inne platformy gier według potrzeb
+];
