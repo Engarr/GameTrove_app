52f9e3d0594a1d2a929aff3397409b1e7b79a15e
Łukasz <lukasz.poczta12@gmail.com>
2023-08-20 16:05:29 +0200

    changed BannerCtx css style

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -198,7 +198,6 @@ export const getUserWishlist = async (req, res, next) => {
         addedAt: matching ? matching.addedAt : null,
       };
     });
-    console.log(combinedList);
     res.status(200).json(combinedList);
   } catch (err) {
     if (!err) {
